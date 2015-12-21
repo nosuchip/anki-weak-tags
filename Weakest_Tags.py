af3f474524312d4e45a29fa8c6d00b680b69adaf
Alex Xela <alp@steppechange.com>
2015-12-21 23:16:24 +0300

    Extended the default to 10 Added availability to ignore some tags Display the tags and their lapses in a table format Total count of cards with lapses added to result window

--- Weakest_Tags.py
+++ Weakest_Tags.py
@@ -6,15 +6,102 @@ from aqt.qt import *
 from collections import defaultdict
 import operator
 
-TOP_WEAKEST = 5
+TOP_WEAKEST = 10
 
 def calculate_and_show_weakest_tags():
-    top, is_ok = QInputDialog.getInteger(mw, _("Top lapses count"),
-                                         _("Number of weakest tags to show (0 means all):"),
-                                        TOP_WEAKEST, 0)
+    class QWeakestTagsQueryDialog(QDialog):
+        def __init__(self, parent=None):
+            QDialog.__init__(self, parent)
 
-    top = top if is_ok else TOP_WEAKEST
+            self.layout = QFormLayout(self)
 
+            self.tags_count_widget = QLineEdit(self)
+            self.tags_count_widget.setInputMask('9999')
+            self.tags_count_widget.setText(str(TOP_WEAKEST))
+            label = QLabel('Number of tags to show:')
+            label.setToolTip('Only number are available')
+            self.layout.addRow(label, self.tags_count_widget)
+
+            self.ignored_tags_widget = QLineEdit(self)
+            label = QLabel('Ignored tags (comma separated):')
+            label.setToolTip('These tags will be excluded from resultset')
+            self.layout.addRow(label, self.ignored_tags_widget)
+
+            button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, parent=self)
+            button_box.accepted.connect(self.accept)
+            button_box.rejected.connect(self.reject)
+            self.layout.addRow(button_box)
+
+        @property
+        def tags_count(self):
+            return int(self.tags_count_widget.text())
+
+        @property
+        def ignored_tags(self):
+            text = self.ignored_tags_widget.text().split()
+            return [t.strip() for t in text if t.strip()]
+
+    class QWeakestTagsResultDialog(QDialog):
+        def __init__(self, parent=None):
+            QDialog.__init__(self, parent)
+
+            width = 500
+            height = 500
+
+            self.setGeometry(
+                parent.x() + parent.width()/2 - width/2,
+                parent.y() + parent.height()/2 - height/2,
+                width, height
+            );
+
+            self.layout = QVBoxLayout(self)
+
+            layout_top = QHBoxLayout()
+            self.cards_lapses_widget = QLabel('?')
+            layout_top.addWidget(QLabel('<b>Total cards with lapses:</b>'))
+            layout_top.addWidget(self.cards_lapses_widget)
+            self.layout.addLayout(layout_top)
+
+            self.layout.addSpacing(10)
+
+            self.layout.addWidget(QLabel('<b>Weakest tags for your deck</b>'))
+
+            self.table_widget = QTableWidget()
+            self.table_widget.horizontalHeader().setResizeMode(QHeaderView.Stretch);
+            self.layout.addWidget(self.table_widget)
+
+            button_box = QDialogButtonBox(QDialogButtonBox.Ok, parent=self)
+            button_box.accepted.connect(self.accept)
+            self.layout.addWidget(button_box)
+
+        def set_cards_with_lapses(self, value):
+            self.cards_lapses_widget.setText(str(value))
+
+        def set_tags_with_lapses(self, values):
+            t = self.table_widget
+
+            t.clear()
+            t.setRowCount(len(values))
+            t.setColumnCount(2)
+
+            t.setHorizontalHeaderLabels(['Tag name', 'Lapses count'])
+
+            try:
+                for row_number, row in enumerate(values):
+                    tag = row[0]
+                    lapses = row[1]
+
+                    t.setItem(row_number, 0, QTableWidgetItem(str(tag)))
+                    t.setItem(row_number, 1, QTableWidgetItem(str(lapses)))
+            except Exception as ex:
+                print "Error occures:", ex
+
+    query_dialog = QWeakestTagsQueryDialog(mw)
+
+    if query_dialog.exec_() == QDialog.Rejected:
+        return
+
+    result_dialog = QWeakestTagsResultDialog(mw)
     current_deck = mw.col.decks.current()
 
     query = '''SELECT
@@ -25,34 +112,39 @@ def calculate_and_show_weakest_tags():
 
     FROM cards c
     INNER JOIN notes n ON c.nid=n.id
-    WHERE c.did={};'''.format(current_deck['id'])
+    WHERE c.did=?;'''
 
     tags_lapses = defaultdict(int)
 
-    for card_id, card_lapses, note_id, tags_str in mw.col.db.execute(query):
+    for card_id, card_lapses, note_id, tags_str in mw.col.db.execute(query, current_deck['id']):
         tags = tags_str.split() if tags_str else []
 
         for tag in tags:
             tags_lapses[tag] += card_lapses
 
     sorted_tags_lapses = sorted(tags_lapses.items(), key=operator.itemgetter(1), reverse=True)
+
+    ignored_tags = query_dialog.ignored_tags
+    sorted_tags_lapses = [t for t in sorted_tags_lapses if t[0] not in ignored_tags]
+
+    top = query_dialog.tags_count
     sorted_tags_lapses = sorted_tags_lapses[:top] if top else sorted_tags_lapses
 
     if sorted_tags_lapses and sorted_tags_lapses[0][1] == 0:
-        message = ('Wow, looks like you have no lapses at all for deck "{}".'
-            ' Congrats!'.format(current_deck['name']))
+       result_dialog.set_tags_with_lapses([('No lapses at all!')])
     else:
-        message = [
-            'Here is your weakes tags  for deck "{}":'.format(current_deck['name']),
-            ''
-        ]
+        result_dialog.set_tags_with_lapses(sorted_tags_lapses)
 
-        for tag, count in sorted_tags_lapses:
-            message.append('{}, lapses {}'.format(tag, count))
+    query = '''SELECT COUNT(c.id) FROM cards c
+    WHERE c.lapses IS NOT NULL AND c.lapses>0 AND c.did=?'''
 
-        message = '\n'.join(message)
+    card_with_lapses = mw.col.db.scalar(query, current_deck['id'])
+    result_dialog.set_cards_with_lapses(card_with_lapses)
 
-    QMessageBox.information(mw, _('Weakest tags'), message)
+    result_dialog.setWindowTitle('Result for deck "{}"'.format(current_deck['name']))
+
+    result_dialog.exec_()
+
 
 action = QAction("Weakest Tags", mw)
 mw.connect(action, SIGNAL("triggered()"), calculate_and_show_weakest_tags)
